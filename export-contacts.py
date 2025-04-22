022e18a53436eaa0db503cf96e2f62c34302bc5e
Matt Pickle <mpickle@aptitude8.com>
2025-04-22 16:02:35 -0500

    Refactor get_contacts function to remove offset parameter and manage offset internally

--- export-contacts.py
+++ export-contacts.py
@@ -1,13 +1,14 @@
 from dotenv import load_dotenv
 import os
 from functions.get_contacts import get_contacts
+from functions.get_lists import get_lists
 from functions.write_to_csv import write_to_csv
 
 load_dotenv()
 API_URL = os.getenv("API_URL")
 API_KEY = os.getenv("API_KEY")
 
-contacts = get_contacts(API_URL, API_KEY, 0)
+contacts = get_contacts(API_URL, API_KEY)
 contact_fieldnames = ["id", "email", "first_name", "last_name", "phone", "address", "city", "state", "zip", "country", "company_name", "company_industry", "company_size", "created_at", "updated_at", "source", "lead_score", "tags"]
 write_to_csv("contacts", contact_fieldnames, contacts)
 
--- functions/get_contacts.py
+++ functions/get_contacts.py
@@ -1,8 +1,9 @@
 import requests
 
+offset = 0
 contacts = []
 
-def get_contacts(API_URL, API_KEY, offset):
+def get_contacts(API_URL, API_KEY):
   url = f"{API_URL}/api/v1/contacts?offset={offset}"
   headers = { "apiKey": API_KEY}
     
@@ -12,8 +13,8 @@ def get_contacts(API_URL, API_KEY, offset):
     print(f"Retrieved contacts: {len(json_response['contacts'])}")
     if json_response["page"] <= json_response["pages"]:
       contacts.extend(json_response["contacts"])
-      next_offset = json_response["page"] * json_response["size"]
-      return get_contacts(API_URL, API_KEY, next_offset)
+      offset = json_response["page"] * json_response["size"]
+      return get_contacts(API_URL, API_KEY)
   except requests.exceptions.RequestException as e:
     print(f"Error getting contacts: {e}")
 
