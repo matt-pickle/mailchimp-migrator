ebb7c6a760b8c1973a7518e88bc2251c50618109
Matt Pickle <mpickle@aptitude8.com>
2025-04-23 14:28:43 -0500

    Fix variable name for storing retrieved contact IDs in get_contact_hs_ids function

--- functions/get_contact_hs_ids.py
+++ functions/get_contact_hs_ids.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-contact_ids = []
+contact_hs_ids = []
 
 def get_contact_hs_ids(PRIVATE_APP_KEY, contact_ids):
   url = "https://api.hubapi.com/crm/v3/objects/contacts/search"
@@ -33,11 +33,11 @@ def get_contact_hs_ids(PRIVATE_APP_KEY, contact_ids):
       json_response = response.json()
       print(f"Retrieved contacts: {len(json_response['results'])}")
       these_ids = [contact["id"] for contact in json_response["results"]]
-      contact_ids.extend(these_ids)
+      contact_hs_ids.extend(these_ids)
     except requests.exceptions.RequestException as e:
       print(f"Error getting contact IDs: {e}")
 
     time.sleep(0.25)
 
-  print(f"Total contact IDs retrieved: {len(contact_ids)}")
-  return contact_ids
+  print(f"Total contact IDs retrieved: {len(contact_hs_ids)}")
+  return contact_hs_ids
