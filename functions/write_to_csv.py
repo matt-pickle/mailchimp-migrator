a5a885b8f5ce1df7d17cdba8e1b3ca4382bc49ef
Matt Pickle <mpickle@aptitude8.com>
2025-04-24 08:51:05 -0500

    Set to QUOTE_ALL to prevent values from separating by spaces

--- functions/write_to_csv.py
+++ functions/write_to_csv.py
@@ -7,7 +7,7 @@ def write_to_csv(title, fieldnames, data):
   csv_filename = os.path.join(os.path.dirname(os.path.dirname(__file__)), "output", f"{title}_{timestamp}.csv")
   if data and len(data) > 0:
     with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
-      writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+      writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
       writer.writeheader()
       writer.writerows(data)
     print(f"Exported {len(data)} {title} to {csv_filename}")
