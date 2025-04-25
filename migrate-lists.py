df7f6e204c2959577ef9e972e410fc5e2c51fce7
Matt Pickle <mpickle@aptitude8.com>
2025-04-25 14:58:44 -0500

    Fix wrong id name for contacts in list

--- migrate-lists.py
+++ migrate-lists.py
@@ -16,7 +16,7 @@ lists = get_lists(API_URL, API_KEY)
 
 for list in lists:
   contacts = get_contacts_in_list(API_URL, API_KEY, list["id"])
-  contact_ids = [contact["id"] for contact in contacts]
+  contact_ids = [contact["contact_id"] for contact in contacts]
   contact_hs_ids = get_contact_hs_ids(PRIVATE_APP_KEY, contact_ids)
   list_hs_id = get_hubspot_list_id(PRIVATE_APP_KEY, list)
   if not list_hs_id:
